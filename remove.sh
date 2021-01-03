b92435faf66d3f88ccfa6910d4a05b29a40ef1cc
Tarsis de Lima <br.tmvdl@gmail.com>
2021-01-02 21:46:04 -0300

    Adding SAMBA and VPN in remove.sh

--- remove.sh
+++ remove.sh
@@ -4,6 +4,11 @@ echo Testing sudo
 sudo echo OK
 
 echo
+echo Removing Samba and VPN
+echo
+sudo apt-get remove --purge -y *samba* *vpn* 
+
+echo
 echo Removing no longer required packages
 echo
 sudo apt-get -y autoremove
