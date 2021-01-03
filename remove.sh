02387ee7e5a38c2ddff3d9574732cb4f1cc293ff
Tarsis de Lima <br.tmvdl@gmail.com>
2021-01-02 23:16:53 -0300

    Adding CUPS in remove.sh

--- remove.sh
+++ remove.sh
@@ -14,6 +14,11 @@ echo
 [[ -e $(which simple-scan) ]] && sudo apt remove --purge -y simple-scan 
 
 echo
+echo Removing CUPS
+echo
+sudo apt remove --purge cups cups-client cups-filters cups-ppdc cups-backend-bjnp cups-common cups-filters-core-drivers cups-server-common cups-browsed cups-core-drivers cups-ipp-utils cups-tea4cups cups-bsd cups-daemon cups-x2go 
+
+echo
 echo Removing no longer required packages
 echo
 sudo apt-get -y autoremove
