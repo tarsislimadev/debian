f6df20b173401e03d8a5ef691bab262de9aff17f
Tarsis de Lima <br.tmvdl@gmail.com>
2021-01-02 21:46:39 -0300

    Adding Simple Scan in remove.sh

--- remove.sh
+++ remove.sh
@@ -9,6 +9,11 @@ echo
 sudo apt-get remove --purge -y *samba* *vpn* 
 
 echo
+echo Removing Simple Scan
+echo
+[[ -e $(which simple-scan) ]] && sudo apt remove --purge -y simple-scan 
+
+echo
 echo Removing no longer required packages
 echo
 sudo apt-get -y autoremove
