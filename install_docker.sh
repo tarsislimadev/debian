798419a20b6bcebef15b9fa62b6e7d8f0d95dc6c
Tarsis de Lima <br.tmvdl@gmail.com>
2021-01-03 17:44:19 -0300

    Adding Docker Compose in install_docker.sh

--- install_docker.sh
+++ install_docker.sh
@@ -16,3 +16,8 @@ echo
 echo Setting up Docker
 echo 
 sudo usermod -aG docker "${USER}"
+echo
+echo Installing Docker Compose
+echo
+sudo curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
