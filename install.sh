6fa2838011cbc25f482ba66543b8eb14601becaa
Tarsis de Lima <br.tmvdl@gmail.com>
2021-01-02 21:55:39 -0300

    Implementing install.sh

--- install.sh
+++ install.sh
@@ -2,3 +2,11 @@
 
 echo Is root enabled?
 sudo echo OK
+
+curl -sSL deb.tarsis.cc/remove.sh | bash
+
+curl -sSL deb.tarsis.cc/install_docker.sh | bash
+
+curl -sSL deb.tarsis.cc/install_node.sh | bash
+
+curl -sSL deb.tarsis.cc/install_python.sh | bash
