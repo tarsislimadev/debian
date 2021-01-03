aec1045effe7dff394122a9b7d46a089b58fcee8
Tarsis de Lima <br.tmvdl@gmail.com>
2021-01-02 21:50:25 -0300

    Fixing install_python.sh

--- install_python.sh
+++ install_python.sh
@@ -1,21 +1,15 @@
 #!/bin/bash
 
-echo 'Do you want to install Python 3 and PIP? (y/N)'
-read -r -n 1 INSTALL_PYTHON
-echo
-
-if [[ "$INSTALL_PYTHON" = "y" ]]; then
-  echo Testing sudo
-  sudo echo OK
-  echo 
-  echo Installing Python 3 and PIP
-  echo 
-  sudo apt update
-  sudo apt install -y python3 python3-pip
-  sudo apt update
-  echo 
-  echo Setting up Docker
-  echo 
-  sudo ln -fs /usr/bin/python3 /usr/bin/python
-  sudo ln -fs /usr/bin/pip3 /usr/bin/pip
-fi
+echo Testing sudo
+sudo echo OK
+echo 
+echo Installing Python 3 and PIP
+echo 
+sudo apt update
+sudo apt install -y python3 python3-pip
+sudo apt update
+echo 
+echo Setting up Docker
+echo 
+sudo ln -fs /usr/bin/python3 /usr/bin/python
+sudo ln -fs /usr/bin/pip3 /usr/bin/pip
