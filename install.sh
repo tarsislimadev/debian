d15efb59a1bf7fef8d3313efe47f2fb91b2bfd5b
Tarsis de Lima <br.tmvdl@gmail.com>
2021-01-05 21:41:51 -0300

    Implementing install_vscode.sh

--- install.sh
+++ install.sh
@@ -10,3 +10,5 @@ curl -sSL deb.tarsis.cc/install_docker.sh | bash
 curl -sSL deb.tarsis.cc/install_node.sh | bash
 
 curl -sSL deb.tarsis.cc/install_python.sh | bash
+
+curl -sSL deb.tarsis.cc/install_vscode.sh | bash
