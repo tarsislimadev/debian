a54bd0b69858a0763e7f8e2d0b455961b38881a7
Tarsis de Lima <br.tmvdl@gmail.com>
2021-01-05 22:51:35 -0300

    DBeaver Installer

--- install.sh
+++ install.sh
@@ -24,3 +24,5 @@ curl -sSL deb.tarsis.cc/install_python.sh | bash
 curl -sSL deb.tarsis.cc/install_vscode.sh | bash
 
 curl -sSL deb.tarsis.cc/git_config.sh | bash
+
+curl -sSL deb.tarsis.cc/install_dbeaver.sh | bash
