330ca455210de172895b8c8b28b8bd83027a6d56
Tarsis de Lima <br.tmvdl@gmail.com>
2021-01-05 22:27:40 -0300

    Implementing git_config.sh

---
+++ git_config.sh
@@ -0,0 +1,3 @@
+
+git config --global user.name "${NAME}"
+git config --global user.email "${EMAIL}"
--- install.sh
+++ install.sh
@@ -3,6 +3,16 @@
 echo Is root enabled?
 sudo echo OK
 
+if [[ -z $NAME ]]; then
+  echo "Please set the \$NAME in ~/.bashrc"
+  exit 1
+fi
+
+if [[ -z $EMAIL ]]; then
+  echo "Please set the \$EMAIL in ~/.bashrc"
+  exit 1
+fi
+
 curl -sSL deb.tarsis.cc/remove.sh | bash
 
 curl -sSL deb.tarsis.cc/install_docker.sh | bash
@@ -12,3 +22,5 @@ curl -sSL deb.tarsis.cc/install_node.sh | bash
 curl -sSL deb.tarsis.cc/install_python.sh | bash
 
 curl -sSL deb.tarsis.cc/install_vscode.sh | bash
+
+curl -sSL deb.tarsis.cc/git_config.sh | bash
