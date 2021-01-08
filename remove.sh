f30cd2d2533dc2e69167b4716535d9aa45a75776
Tarsis de Lima <br.tmvdl@gmail.com>
2021-01-08 15:13:25 -0300

    Adding Printer Drivers in remove.sh

--- remove.sh
+++ remove.sh
@@ -14,6 +14,11 @@ echo
 [[ -e $(which simple-scan) ]] && sudo apt remove --purge -y simple-scan 
 
 echo
+echo Removing Printer Drivers
+echo
+sudo apt remove --purge  printer-driver-brlaser printer-driver-gutenprint printer-driver-pnm2ppa printer-driver-sag-gdi printer-driver-c2esp printer-driver-hpcups printer-driver-postscript-hp printer-driver-splix printer-driver-foo2zjs printer-driver-m2300w printer-driver-ptouch printer-driver-foo2zjs-common printer-driver-min12xxw printer-driver-pxljr 
+
+echo
 echo Removing no longer required packages
 echo
 sudo apt-get -y autoremove
