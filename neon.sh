61ec6df010ea09acaccac4aff53c857516d73857
Tarsis de Lima <br.tmvdl@gmail.com>
2021-01-26 19:02:33 +0000

    Adding installer for Neon Live

---
+++ neon.sh
@@ -0,0 +1,24 @@
+sudo apt update -y 
+# installing Brave
+sudo apt install -y apt-transport-https curl gnupg
+curl -s https://brave-browser-apt-release.s3.brave.com/brave-core.asc | sudo apt-key --keyring /etc/apt/trusted.gpg.d/brave-browser-release.gpg add -
+echo "deb [arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main" | sudo tee /etc/apt/sources.list.d/brave-browser-release.list
+sudo apt update -y
+sudo apt install -y brave-browser
+# installing Node.js (current)
+curl -sL https://deb.nodesource.com/setup_current.x | sudo -E bash -
+sudo apt install -y gcc g++ make
+curl -sL https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+sudo apt update && sudo apt install -y yarn
+sudo apt install -y nodejs
+# installing VSCode
+wget 'https://code.visualstudio.com/sha/download?build=stable&os=linux-deb-x64'
+mv download\?build\=stable\&os\=linux-deb-x64 code.deb
+sudo apt install -y ./code.deb
+# installing Git
+sudo apt install -y git
+# remove
+sudo apt remove --purge -y bluedevil bluez bluez-cups bluez-obexd
+sudo apt remove --purge -y gwenview ark okular-backends kdeconnect plasma-discover-common vlc
+sudo apt remove --purge -y firefox snapd 
