b6fded96059ffeb33cdf04cc1054c320ec956f14
Tarsis de Lima <br.tmvdl@gmail.com>
2021-03-10 20:59:20 -0300

    Adding Brave installer

---
+++ install_brave.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+echo Testing sudo
+sudo echo OK
+echo 
+echo Installing Brave
+echo 
+sudo apt update
+sudo apt install -y apt-transport-https curl gnupg
+curl -s https://brave-browser-apt-release.s3.brave.com/brave-core.asc | sudo apt-key --keyring /etc/apt/trusted.gpg.d/brave-browser-release.gpg add -
+echo "deb [arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main" | sudo tee /etc/apt/sources.list.d/brave-browser-release.list
+sudo apt update
+sudo apt install -y brave-browser
+echo
+echo Brave version
+echo
+brave-browser --version 
