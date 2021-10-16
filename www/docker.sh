c823f4cc63b89b17c077c7398f02abb73478bfd4
Tarsis de Lima <br.tmvdl@gmail.com>
2021-10-16 12:26:01 -0300

    Fixing instalation of Docker and Docker Compose

--- www/docker.sh
+++ www/docker.sh
@@ -10,17 +10,17 @@ sudo apt install -y apt-transport-https ca-certificates curl software-properties
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
 sudo apt update
-apt-cache policy docker-ce
-sudo apt install -y docker-ce docker-compose
+# apt-cache policy docker-ce
+sudo apt install -y docker-ce docker-compose docker.io containerd  docker-ce-cli docker-ce-rootless-extras docker-compose docker-scan-plugin
 echo 
 echo Setting up Docker
 echo 
 sudo usermod -aG docker "${USER}"
 echo
-echo Installing Docker Compose v2.0.1
-echo
-sudo curl -L "https://github.com/docker/compose/releases/download/v2.0.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
+# echo Installing Docker Compose v2.0.1
+# echo
+# sudo curl -L "https://github.com/docker/compose/releases/download/v2.0.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+# sudo chmod +x /usr/local/bin/docker-compose
 echo
 echo Docker version
 echo
