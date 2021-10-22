1df279d7a82c7d0cf4ff65ffb5b053d20606949b
Tarsis de Lima <br.tmvdl@gmail.com>
2021-10-22 11:32:07 -0300

    Fixing Docker installer

--- www/docker.sh
+++ www/docker.sh
@@ -2,25 +2,21 @@
 
 echo Testing sudo
 sudo echo OK
-echo 
+echo
 echo Installing Docker
-echo 
+echo
 sudo apt update
 sudo apt install -y apt-transport-https ca-certificates curl software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
 sudo apt update
-# apt-cache policy docker-ce
-sudo apt install -y docker-ce docker-compose docker.io containerd  docker-ce-cli docker-ce-rootless-extras docker-compose docker-scan-plugin
-echo 
+apt-cache policy docker-ce
+sudo apt install -y docker-ce docker-compose
+echo
 echo Setting up Docker
-echo 
+echo
 sudo usermod -aG docker "${USER}"
 echo
-# echo Installing Docker Compose v2.0.1
-# echo
-# sudo curl -L "https://github.com/docker/compose/releases/download/v2.0.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-# sudo chmod +x /usr/local/bin/docker-compose
 echo
 echo Docker version
 echo
