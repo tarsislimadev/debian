d3a592685ea768635782150bf10d76fc9f6c78eb
Tarsis de Lima <br.tmvdl@gmail.com>
2021-10-27 16:01:55 -0300

    Fixing Docker installer

--- www/docker.sh
+++ www/docker.sh
@@ -3,15 +3,18 @@
 echo Testing sudo
 sudo echo OK
 echo
-echo Installing Docker
+echo Removing older Docker installations
 echo
-sudo apt update
-sudo apt install -y apt-transport-https ca-certificates curl software-properties-common
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
-sudo apt update
-apt-cache policy docker-ce
-sudo apt install -y docker-ce docker-compose
+sudo apt remove --purge -y docker-ce docker-ce-cli docker-ce-rootless-extras docker-compose containerd.io
+echo
+echo Installing Docker and Docker Compose
+echo
+sudo apt-get update
+sudo apt-get install -y ca-certificates curl gnupg lsb-release
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+sudo apt-get install -y docker-compose
 echo
 echo Setting up Docker
 echo
