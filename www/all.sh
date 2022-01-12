21e696a1fc5d2ab7487d0a440ade73e5574c2341
Tarsis de Lima <br.tmvdl@gmail.com>
2022-01-12 17:10:25 -0300

    Removing Insomnia and Python installers

--- www/all.sh
+++ www/all.sh
@@ -4,10 +4,6 @@ curl -sSL deb.tarsis.cc/min.sh | bash
 
 curl -sSL deb.tarsis.cc/docker.sh | bash
 
-curl -sSL deb.tarsis.cc/python.sh | bash
-
 curl -sSL deb.tarsis.cc/dbeaver.sh | bash
 
 curl -sSL deb.tarsis.cc/gh.sh | bash
-
-curl -sSL deb.tarsis.cc/insomnia.sh | bash
