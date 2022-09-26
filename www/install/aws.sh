09f633ab6edda1255480fafe65e32554bebeb347
Tarsis de Lima <br.tmvdl@gmail.com>
2022-09-26 10:25:54 -0300

    Updating AWS CLI installer

--- www/install/aws.sh
+++ www/install/aws.sh
@@ -6,6 +6,9 @@ sudo echo OK
 echo
 echo Installing AWS CLI
 echo
-curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
-unzip awscliv2.zip
-sudo ./aws/install
+sudo apt install -y awscli
+
+echo
+echo Version of AWS CLI
+echo
+awscli --version
