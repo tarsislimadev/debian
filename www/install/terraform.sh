d8e932e18312564e344306ae84dd221999a695cd
Tarsis de Lima <br.tmvdl@gmail.com>
2022-11-11 17:39:56 -0300

    Fixing Terraform installer

--- www/install/terraform.sh
+++ www/install/terraform.sh
@@ -6,6 +6,12 @@ sudo echo OK
 echo
 echo Install Terraform
 echo
-curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
-sudo apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
-sudo apt-get update && sudo apt-get install terraform
+wget -O- https://apt.releases.hashicorp.com/gpg | gpg --dearmor | sudo tee /usr/share/keyrings/hashicorp-archive-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
+sudo apt -y update
+sudo apt install -y terraform
+
+echo
+echo Version of Terraform
+echo
+terraform --version
