957225a760d4b741ba0194807a59d109c830188c
Tarsis <br.tmvdl@gmail.com>
2022-12-16 17:32:34 -0300

    Fixing bash script

--- www/install/vscode.sh
+++ www/install/vscode.sh
@@ -8,18 +8,12 @@ echo Installing VSCode
 echo
 
 sudo apt -y update
-$VSCODE_PATH=$HOME/code.deb
+VSCODE_PATH=$HOME/code.deb
 
-curl 'https://code.visualstudio.com/sha/download?build=stable&os=linux-deb-x64' \
-    -H 'Connection: keep-alive' \
-    -H 'Upgrade-Insecure-Requests: 1' \
-    -H 'User-Agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.61 Safari/537.36' \
-    -H 'Accept-Language: en-US,en;q=0.9,pt-BR;q=0.8,pt;q=0.7' \
-    --compressed \
-    --output $VSCODE_FILE
+curl -sSL https://code.visualstudio.com/sha/download?build=stable&os=linux-deb-x64 --compressed --output $VSCODE_PATH
 
-sudo apt install -y $VSCODE_FILE
-rm -rf $VSCODE_FILE
+sudo apt install -y $VSCODE_PATH
+rm -rf $VSCODE_PATH
 
 echo
 echo VSCode version
