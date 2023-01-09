2568a10c1f30acc2cb226b528fcab18f0795e4dd
Tarsis <br.tmvdl@gmail.com>
2023-01-09 14:55:46 -0300

    Adding Flutter installer file

---
+++ src/install/flutter.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+echo Testing sudo
+sudo echo OK
+
+echo
+echo Removing older Flutter
+echo
+
+echo
+echo Installing Flutter
+echo
+sudo apt-get update
+wget 'https://storage.googleapis.com/flutter_infra_release/releases/stable/linux/flutter_linux_3.3.10-stable.tar.xz'
+tar -xf 'flutter_linux_3.3.10-stable.tar.xz'
+mv flutter/ $HOME/.flutter/
+
+echo
+echo Setting up Flutter
+echo
+echo "declare -x PATH=$PATH:$HOME/.flutter/bin " >> $HOME/.bashrc
+
+echo
+echo
+echo Flutter version
+echo
+
