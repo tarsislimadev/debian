cd020217d21f457de6b3a3e63a0897d1421690d3
Tarsis <br.tmvdl@gmail.com>
2023-03-03 20:32:42 -0300

    Adding Unity Hub installer

---
+++ src/install/unityhub.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo Testing sudo
+sudo echo OK
+
+echo
+echo Installing Unity Hub
+echo
+sudo apt -y update
+sudo sh -c 'echo "deb https://hub.unity3d.com/linux/repos/deb stable main" > /etc/apt/sources.list.d/unityhub.list'
+wget -qO - https://hub.unity3d.com/linux/keys/public | sudo apt-key add -
+sudo apt update
+sudo apt-get install unityhub
