2d302419f602f1ebc09c20a92dc4dcf90e1bd56e
Tarsis <br.tmvdl@gmail.com>
2023-03-12 18:22:22 -0300

    Adding google Chrome installer

---
+++ src/install/google-chrome.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+FILENAME="google-chrome-stable_current_amd64.deb"
+
+echo Testing sudo
+sudo echo OK
+
+echo
+echo Installing Google Chrome
+echo
+sudo apt update -y
+curl -sSL "https://dl.google.com/linux/direct/${FILENAME}" \
+  -H "user-agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36" \
+  -H "accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8" \
+  -H "referer: https://www.google.com/" \
+  -H "accept-language: en-US,en;q=0.9" \
+  -H "upgrade-insecure-requests: 1" \
+  -H "authority: dl.google.com" \
+  --compressed --output "${FILENAME}"
+sudo apt update -y
+sudo apt install -y "./${FILENAME}"
+rm -rf "./${FILENAME}"
+
+echo
+echo Google Chrome version
+echo
+google-chrome-stable --version
