ff4208b081e8dc480ecc6254b40192673c2c43df
Tarsis <br.tmvdl@gmail.com>
2023-04-19 17:17:32 -0300

    Fixing Youtube installer

---
+++ src/install/yt-dlp.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo Testing sudo
+sudo echo OK
+
+echo
+echo Installing YT DLP
+echo
+sudo apt update -y
+sudo apt install -y python3-pip
+sudo python3 -m pip install --force-reinstall 'https://github.com/yt-dlp/yt-dlp/archive/master.tar.gz'
