4c109bba0fb77ee686b0c7ad758a36001f860dc9
Tarsis <br.tmvdl>
2023-04-20 20:02:40 -0300

    Fixing Youtube installer

--- src/install/yt-dlp.sh
+++ src/install/yt-dlp.sh
@@ -7,5 +7,5 @@ echo
 echo Installing YT DLP
 echo
 sudo apt update -y
-sudo apt install -y python3-pip
+sudo apt install -y python3-pip ffmpeg
 sudo python3 -m pip install --force-reinstall 'https://github.com/yt-dlp/yt-dlp/archive/master.tar.gz'
