118918d2b4417b890a91095d74f56d7a6ed03cec
Tarsis <br.tmvdl@gmail.com>
2023-06-04 14:18:43 -0300

    Updating Node.js installer

--- src/install/node.sh
+++ src/install/node.sh
@@ -7,8 +7,8 @@ echo
 echo Installing Node.js LTS
 echo
 sudo apt-get update
-curl -sL https://deb.nodesource.com/setup_lts.x | sudo -E bash -
-sudo apt-get install nodejs
+curl -fsSL https://deb.nodesource.com/setup_lts.x | sudo -E bash -
+sudo apt-get install -y nodejs
 
 echo
 echo Installing development tools
@@ -18,11 +18,8 @@ sudo apt-get install gcc g++ make
 echo
 echo Installing Yarn package manager
 echo
-curl -sL https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
-echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
-sudo apt-get update
-sudo apt-get install yarn
-sudo apt-get update
+sudo apt-get -y update
+sudo apt-get -y install yarn
 
 echo node --version
 node --version
