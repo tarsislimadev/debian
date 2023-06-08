7f2ad05c80b778c179a9c7a659baadc44536254c
Tarsis <br.tmvdl@gmail.com>
2023-06-08 15:12:43 -0300

    Adding "Minimal installer" file

---
+++ src/min.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+bash ./install/brave.sh
+
+bash ./install/gh.sh
+
+bash ./install/git.sh
+
+bash ./install/google-chrome.sh
+
+bash ./install/jq.sh
+
+bash ./install/node.sh
+
+bash ./install/vscode.sh
