bc5944e3467e7d6b095ea94a1907588a156b8b43
Tarsis <br.tmvdl@gmail.com>
2023-06-28 11:29:53 -0300

    Updating Stripe installer file

---
+++ src/install/stripe.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo Testing sudo
+sudo echo OK
+
+echo
+echo
+echo Installing Stripe
+echo
+curl -s https://packages.stripe.dev/api/security/keypair/stripe-cli-gpg/public | gpg --dearmor | sudo tee /usr/share/keyrings/stripe.gpg
+echo "deb [signed-by=/usr/share/keyrings/stripe.gpg] https://packages.stripe.dev/stripe-cli-debian-local stable main" | sudo tee -a /etc/apt/sources.list.d/stripe.list
+sudo apt -y update
+sudo apt -y install stripe
