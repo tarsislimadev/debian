bc274e7206b243c04a269245b131e341c1e8494a
Tarsis <br.tmvdl@gmail.com>
2023-08-14 07:13:18 -0300

    Updating min.sh file

--- src/min.sh
+++ src/min.sh
@@ -6,10 +6,6 @@ bash ./install/gh.sh
 
 bash ./install/git.sh
 
-bash ./install/google-chrome.sh
-
-bash ./install/jq.sh
-
-bash ./install/node.sh
+bash ./install/docker.sh
 
 bash ./install/vscode.sh
