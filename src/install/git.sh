4204b91ee7dbdce126afffa22f3a6d7b8701d688
Tarsis <br.tmvdl@gmail.com>
2023-10-18 10:40:53 -0300

    Updating Git installer file

--- src/install/git.sh
+++ src/install/git.sh
@@ -2,12 +2,18 @@
 
 echo Testing sudo
 sudo echo OK
+echo
 
 echo
 echo Installing Git
 echo
 sudo apt -y update
 sudo apt -y install git make
+
+echo
+echo Configuration of Git
+echo
+git config --global init.defaultBranch main
 
 echo
 echo Git version
