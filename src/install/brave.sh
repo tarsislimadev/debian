a9db6191e914cbf4459e04d9ffde3651ad4deb63
Tarsis <br.tmvdl@gmail.com>
2024-06-04 13:55:43 -0300

    Updating app files

--- src/install/brave.sh
+++ src/install/brave.sh
@@ -8,16 +8,16 @@ echo Installing Brave
 echo
 sudo apt -y update
 sudo apt -y install apt-transport-https curl gnupg
-curl -s https://brave-browser-apt-release.s3.brave.com/brave-core.asc | sudo apt-key --keyring /etc/apt/trusted.gpg.d/brave-browser-release.gpg add -
-echo "deb [arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main" | sudo tee /etc/apt/sources.list.d/brave-browser-release.list
+sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
 sudo apt -y update
 sudo apt -y install brave-browser
-
 
 echo
 echo Setting up Brave
 echo
 echo 'alias b="brave-browser-stable & " ' >> ~/.bashrc 
+echo
 
 echo
 echo Brave version
