7594b27b97d08d76e3b989ceabeed96759cbb8ce
Tarsis <br.tmvdl@gmail.com>
2024-07-10 18:20:49 -0300

    Updating app files

--- src/install/nvm.sh
+++ src/install/nvm.sh
@@ -6,10 +6,10 @@ sudo echo OK
 echo
 echo Installing NVM
 echo
-curl -s -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
 nvm install 20
 
 echo
 echo NVM version
 echo
-node --version
+node -v 
