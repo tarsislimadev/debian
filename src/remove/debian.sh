f9ee2c3f510237ee20211099f977e51e53b32426
Tarsis Lima <br.tmvdl@gmail.com>
2024-09-25 21:09:33 +0000

    Updating app files

--- src/remove/debian.sh
+++ src/remove/debian.sh
@@ -4,10 +4,8 @@ sudo netstat --tcp --udp --listening --all --numeric --program --verbose
 sudo systemctl stop kdeconnect
 sudo systemctl disable kdeconnect
 
-sudo apt remove --purge cups
-sudo apt remove --purge kdeconnect
-sudo apt remove --purge exim4
-
-sudo apt autoremove --purge
-
 sudo killall exim4 kdeconnectd
+
+sudo apt -y remove --purge cups kdeconnect exim4 bluetooth
+
+sudo apt -y autoremove --purge
