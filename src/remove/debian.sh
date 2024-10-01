4baa646efa9a176d52fa0e117dda190a6876fca5
Tarsis Lima <br.tmvdl@gmail.com>
2024-10-01 18:44:40 +0000

    Updating app files

--- src/remove/debian.sh
+++ src/remove/debian.sh
@@ -6,6 +6,6 @@ sudo systemctl disable kdeconnect
 
 sudo killall exim4 kdeconnectd
 
-sudo apt -y remove --purge cups* kdeconnect* exim4* bluetooth* xwayland* kwin-wayland* plasma-workspace-wayland* xwayland* dragonplayer*
+sudo apt -y remove --purge cups* kdeconnect* exim4* bluetooth* xwayland* kwin-wayland* plasma-workspace-wayland* xwayland* dragonplayer* okular*
 
 sudo apt -y autoremove --purge
