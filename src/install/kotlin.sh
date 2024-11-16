40c8f4320cf7d520d83428c02b6513d424a2f238
Tarsis Lima <69215425+tarsislimadev@users.noreply.github.com>
2024-11-16 16:08:27 -0300

    Updating app files

--- src/install/kotlin.sh
+++ src/install/kotlin.sh
@@ -1,1 +1,27 @@
-echo Install kotlinc
+#!/bin/bash
+
+echo Testing sudo
+sudo echo OK
+
+echo
+echo Install kotlin compiler
+echo
+
+VERSION="2.0.21"
+
+sudo apt -y update
+sudo apt -y install wget zip
+
+wget "https://github.com/JetBrains/kotlin/releases/download/v${VERSION}/kotlin-compiler-${VERSION}.zip"
+unzip "kotlin-compiler-${VERSION}.zip"
+rm -rf "kotlin-compiler-${VERSION}.zip"
+
+sudo mv kotlinc /usr/share/kotlinc
+
+echo 'PATH="${PATH}":/usr/share/kotlinc/bin' >> ~/.bashrc
+
+echo
+echo Install kotlin compiler
+echo
+
+kotlinc -version
