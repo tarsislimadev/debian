f46674173c2693f0600c15a4bee40eb8fcd8e1b9
Tarsis Lima <69215425+tarsislimadev@users.noreply.github.com>
2024-11-17 20:12:31 -0300

    Updating app files

--- src/run/ffmpeg/cam+desktop+mic.sh
+++ src/run/ffmpeg/cam+desktop+mic.sh
@@ -8,4 +8,4 @@ ffmpeg \
  -f v4l2 -i /dev/video0 \
  -f alsa -ac 2 -i "" \
  -filter_complex '[0]scale=1280:720[a0];[1]scale=128:72[b1];[b1]pad=1280:720:1140:640:0x00000000[a1];[a0][a1]overlay' \
- -f flv "cam+desktop+mic.${datetime}.flv"
+ -q:v 1 -f flv "cam+desktop+mic.${datetime}.flv"
