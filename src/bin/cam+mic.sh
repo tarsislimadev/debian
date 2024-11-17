c605f250ee33fb43d9ef6daf07e9b9029097ea1f
Tarsis Lima <br.tmvdl@gmail.com>
2024-11-17 10:42:23 -0300

    Updating app files

--- src/bin/cam+mic.sh
+++ src/bin/cam+mic.sh
@@ -1,3 +1,3 @@
 datetime=$( date +%Y%m%d%H%M%S )
 
-ffmpeg -f v4l2 -i "/dev/video0" -f alsa -i "" "cam.${datetime}.mkv"
+ffmpeg -f v4l2 -i "/dev/video0" -f alsa -i "" -f flv "cam+mic.${datetime}.flv"
--- src/run/ffmpeg/cam+desktop+mic.sh
+++ src/run/ffmpeg/cam+desktop+mic.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/sh
 
-. ~/.env
-
 datetime=$( date +%Y%m%d%H%M%S )
 
 ffmpeg \
@@ -10,4 +8,4 @@ ffmpeg \
  -f v4l2 -i /dev/video0 \
  -f alsa -ac 2 -i "" \
  -filter_complex '[0]scale=1280:720[a0];[1]scale=128:72[b1];[b1]pad=1280:720:1140:640:0x00000000[a1];[a0][a1]overlay' \
- "cam+desktop+mic.${datetime}.mkv"
+ -f flv "cam+desktop+mic.${datetime}.flv"
