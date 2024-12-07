c294973cd906b6b6b6e2731ed4796fc1494f5e39
Tarsis Lima <69215425+tarsislimadev@users.noreply.github.com>
2024-12-07 12:08:30 -0300

    Updating app files

--- src/install/webstorm.sh
+++ src/install/webstorm.sh
@@ -1,3 +1,9 @@
 
 
-# webstorm
+FILENAME="WebStorm-2024.3.1.tar.gz"
+
+curl -O "https://download-cdn.jetbrains.com/webstorm/${FILENAME}"
+
+tar -xf WebStorm-2024.3.1.tar.gz
+
+sudo mv WebStorm-243.22562.112/ /usr/bin/WebStorm/
