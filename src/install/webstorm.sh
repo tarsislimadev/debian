24c99d0f71bb4cbb812923b67fe4879f4e5d7f93
Tarsis Lima <69215425+tarsislimadev@users.noreply.github.com>
2024-12-07 12:09:51 -0300

    Updating app files

--- src/install/webstorm.sh
+++ src/install/webstorm.sh
@@ -1,9 +1,13 @@
+#!/bin/bash
 
+echo Testing sudo
+sudo echo OK
+
+echo
+echo Installing VSCode
+echo
 
 FILENAME="WebStorm-2024.3.1.tar.gz"
-
 curl -O "https://download-cdn.jetbrains.com/webstorm/${FILENAME}"
-
-tar -xf WebStorm-2024.3.1.tar.gz
-
+tar -xf "${FILENAME}"
 sudo mv WebStorm-243.22562.112/ /usr/bin/WebStorm/
