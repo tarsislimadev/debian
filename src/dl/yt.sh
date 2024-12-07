da2c11eb8117526374c90d73c9958dc94a8912b6
Tarsis Lima <69215425+tarsislimadev@users.noreply.github.com>
2024-12-07 20:59:46 -0300

    Updating app files

--- src/dl/yt.sh
+++ src/dl/yt.sh
@@ -1,5 +1,1 @@
-
-docker run --rm -d \
-  -v $(pwd):/app -w /app \
-  --network host tmvdl/yt-dl \
-  youtube-dl -o '%(uploader)s/%(playlist)s/%(playlist_index)s - %(title)s.%(ext)s' $@
+docker run --rm -d -v $(pwd):/app -w /app --network host tmvdl/yt-dl youtube-dl -o '%(uploader)s/%(playlist)s/%(playlist_index)s - %(title)s.%(ext)s' "${@}"
