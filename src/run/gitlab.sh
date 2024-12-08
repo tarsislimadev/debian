637a5ae5d645e3950cc36c7dd1a24cfd6ce2898c
Tarsis Lima <69215425+tarsislimadev@users.noreply.github.com>
2024-12-08 20:24:35 -0300

    Updating app files

--- src/run/gitlab.sh
+++ src/run/gitlab.sh
@@ -1,12 +1,14 @@
+#!/usr/bin/sh
 
-# RUNNER_TOKEN=
+. ~/.gitlab_runner_token
 
 docker run --rm -it --name gitlab-runner \
   -v /srv/gitlab-runner/config:/etc/gitlab-runner \
   -v /var/run/docker.sock:/var/run/docker.sock \
-  gitlab/gitlab-runner:alpine  register \
+  gitlab/gitlab-runner:alpine \
+  register \
   --executor "docker" \
-  --token "$RUNNER_TOKEN" \
+  --token "${gitlab_runner_token}" \
   --docker-image alpine:latest \
   --description "docker-runner" \
   --url "https://gitlab.com"
