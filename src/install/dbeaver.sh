6ef82227fd20563f6511be881ca782fcc05cebc6
Tarsis Lima <69215425+tarsislimadev@users.noreply.github.com>
2024-12-07 21:04:49 -0300

    Updating app files

--- src/install/dbeaver.sh
+++ src/install/dbeaver.sh
@@ -7,11 +7,13 @@ echo
 echo Installing DBeaver Community Edition
 echo
 
+FILENAME="dbeaver-ce_latest_amd64.deb"
+
 sudo apt -y update
 sudo apt -y install wget
-wget https://dbeaver.io/files/dbeaver-ce_latest_amd64.deb
+curl -O "https://dbeaver.io/files/${FILENAME}"
+rm -rf "${FILENAME}"
 
 echo
 [[ -e "$(which dbeaver)" ]] && echo DBeaver Installed
 echo
-
