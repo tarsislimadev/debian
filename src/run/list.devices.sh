64eb94ef59c0f734d90e2cf6e9c78657ef2e07dd
Tarsis Lima <69215425+tarsislimadev@users.noreply.github.com>
2024-12-08 20:21:52 -0300

    Updating app files

--- src/run/list.devices.sh
+++ src/run/list.devices.sh
@@ -1,5 +1,9 @@
 echo wait
 
-for i in $( seq 1 10 ) ; do echo $( ping -c 1 "192.168.15.${i}" | cut -d ' '  -f4 - ) >> devices ; echo "${i} done"  ; done ;
+echo > ./devices
+
+for i in $( seq 1 10 ) ; do echo $( ping -c 1 "192.168.15.${i}" | cut -d ' '  -f 4 - | cut -d ' ' -f 2 ) >> ./devices ; echo "${i} done"  ; done ;
+
+cat ./devices
 
 echo ready
