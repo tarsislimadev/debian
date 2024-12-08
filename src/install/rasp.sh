65f819e7c1ea386d13193b83b601a9f68344b2c5
Tarsis Lima <69215425+tarsislimadev@users.noreply.github.com>
2024-12-07 21:22:57 -0300

    Updating app files

--- src/install/rasp.sh
+++ src/install/rasp.sh
@@ -6,12 +6,12 @@ sudo echo OK
 echo
 echo Installing Raspberry Pi Imager
 echo
-wget 'https://downloads.raspberrypi.org/imager/imager_latest_amd64.deb'
-sudo apt -y install ./imager_latest_amd64.deb
+FILENAME="imager_latest_amd64.deb"
+curl -O "https://downloads.raspberrypi.org/imager/${FILENAME}"
+sudo apt -y install "${FILENAME}"
 sudo apt -y install rpi-imager
 
 echo
 echo Downloading Raspberry Pi Desktop OS
 echo
-wget http://www.cdimage.ubuntu.com/ubuntu/releases/22.04/release/ubuntu-22.04.1-preinstalled-server-armhf+raspi.img.xz
-
+curl -O 'http://www.cdimage.ubuntu.com/ubuntu/releases/22.04/release/ubuntu-22.04.1-preinstalled-server-armhf+raspi.img.xz'
