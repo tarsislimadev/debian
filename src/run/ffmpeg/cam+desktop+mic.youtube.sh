3dac7fba32e3138c7d1e6a31e4049e53d6cd489c
Tarsis Lima <69215425+tarsislimadev@users.noreply.github.com>
2024-12-08 16:48:29 -0300

    Updating app files

---
+++ src/run/ffmpeg/cam+desktop+mic.youtube.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/sh
+
+datetime=$( date +%Y%m%d%H%M%S )
+
+. ~/.youtube
+
+ffmpeg \
+ -s 1280x720 \
+ -f x11grab -i "${DISPLAY}" \
+ -f v4l2 -i /dev/video0 \
+ -f alsa -ac 2 -i "" \
+ -filter_complex '[0]scale=1280:720[a0];[1]scale=128:72[b1];[b1]pad=1280:720:1140:640:0x00000000[a1];[a0][a1]overlay' \
+ -q:v 1 -f flv "rtmp://x.rtmp.youtube.com/live2/${youtube_key}"
