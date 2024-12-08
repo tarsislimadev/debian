d86c2233dbd6e745f39ea0ac6736285722702b21
Tarsis Lima <69215425+tarsislimadev@users.noreply.github.com>
2024-12-07 21:10:02 -0300

    Updating app files

--- src/install/gradle.sh
+++ src/install/gradle.sh
@@ -3,13 +3,11 @@
 echo Testing sudo
 sudo echo OK
 
-FILENAME=gradle-7.3.3-bin.zip
-
 echo
 echo Installing Gradle
 sudo apt -y update
-curl -sSL 'https://services.gradle.org/distributions/${FILENAME}' --output $FILENAME
+FILENAME="gradle-7.3.3-bin.zip"
+curl -sSL 'https://services.gradle.org/distributions/${FILENAME}' --output "${FILENAME}"
 mkdir -p ~/.gradle
-unzip -d ~/.gradle $FILENAME
-rm -rf $FILENAME
-
+unzip -d ~/.gradle "${FILENAME}"
+rm -rf "${FILENAME}"
