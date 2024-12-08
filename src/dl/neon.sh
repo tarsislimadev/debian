e4d3c5cc5b0116880a81921ce41c1ad4307a5123
Tarsis Lima <69215425+tarsislimadev@users.noreply.github.com>
2024-12-07 21:00:11 -0300

    Updating app files

--- src/dl/neon.sh
+++ src/dl/neon.sh
@@ -1,1 +1,1 @@
-wget 'ftp://www-ftp.lip6.fr/pub/X11/kde-applicationdata/neon/images/user/20220512-0952/neon-user-20220512-0952.iso'
+curl -O 'ftp://www-ftp.lip6.fr/pub/X11/kde-applicationdata/neon/images/user/20220512-0952/neon-user-20220512-0952.iso'
