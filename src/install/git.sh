cd0e75864c728d1658a255aa70fb8d36403bb3cb
Tarsis Lima <69215425+tarsislimadev@users.noreply.github.com>
2024-12-07 21:08:31 -0300

    Updating app files

--- src/install/git.sh
+++ src/install/git.sh
@@ -13,6 +13,12 @@ sudo apt -y install git make
 echo
 echo Configuration of Git
 echo
+echo "name: "
+read name
+git config --global user.name "${name}"
+echo "email: "
+read email
+git config --global user.email "${email}"
 git config --global init.defaultBranch main
 
 echo
