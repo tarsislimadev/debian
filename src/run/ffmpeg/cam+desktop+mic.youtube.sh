359aac5cb544f802e70898d3eec4bd0fbb04a805
Tarsis Lima <69215425+tarsislimadev@users.noreply.github.com>
2024-12-08 20:02:07 -0300

    Updating app files

--- src/run/ffmpeg/cam+desktop+mic.youtube.sh
+++ src/run/ffmpeg/cam+desktop+mic.youtube.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/sh
 
-datetime=$( date +%Y%m%d%H%M%S )
+. ~/.datetime
 
-. ~/.youtube
+. ~/.youtube_key
+
+echo
+echo "datetime: ${datetime}"
+echo
 
 ffmpeg \
  -s 1280x720 \
@@ -10,4 +14,6 @@ ffmpeg \
  -f v4l2 -i /dev/video0 \
  -f alsa -ac 2 -i "" \
  -filter_complex '[0]scale=1280:720[a0];[1]scale=128:72[b1];[b1]pad=1280:720:1140:640:0x00000000[a1];[a0][a1]overlay' \
- -q:v 1 -f flv "rtmp://x.rtmp.youtube.com/live2/${youtube_key}"
+ -q:v 1 \
+ -f flv "rtmp://x.rtmp.youtube.com/live2/${youtube_key}" \
+ -f flv "x11grab.v4l2.alsa.${datetime}.flv"
