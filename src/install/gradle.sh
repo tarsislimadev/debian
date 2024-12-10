a64a6f100caf87c109ddc9f61eb81f6033923fa8
Tarsis Lima <69215425+tarsislimadev@users.noreply.github.com>
2024-12-10 16:03:24 -0300

    Updating app files

--- src/install/gradle.sh
+++ src/install/gradle.sh
@@ -7,7 +7,7 @@ echo
 echo Installing Gradle
 sudo apt -y update
 FILENAME="gradle-8.11.1-bin.zip"
-curl -sSL 'https://services.gradle.org/distributions/${FILENAME}' --output "${FILENAME}"
+curl -O 'https://services.gradle.org/distributions/${FILENAME}'
 mkdir -p ~/.gradle
 unzip -d ~/.gradle "${FILENAME}"
 rm -rf "${FILENAME}"
