918bd4584b6c01e8c727a526f7275ea281ebd7e6
Tarsis Lima <69215425+tarsislimadev@users.noreply.github.com>
2024-12-10 17:34:51 -0300

    Updating app files

--- src/bin/ffmpeg.desktop.mic.sh
+++ src/bin/ffmpeg.desktop.mic.sh
@@ -1,5 +1,7 @@
-#!/usr/bin/sh
+. ~/.datetime
 
-datetime=$( date +%Y%m%d%H%M%S )
-
-ffmpeg -f x11grab -i "${DISPLAY}" -f alsa -i "" -q:v 1 -f flv "desktop+mic.${datetime}.flv"
+ffmpeg -y \
+ -f x11grab -i "${DISPLAY}" \
+ -f alsa -i "" \
+ -q:v 1 \
+ -f flv "desktop+mic.${datetime}.flv"
