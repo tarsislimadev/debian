678f6cf9df12dd9c8c348f8afc8ab5677fb0b958
Tarsis Lima <69215425+tarsislimadev@users.noreply.github.com>
2024-12-10 17:35:55 -0300

    Updating app files

--- src/bin/ffmpeg.cam.mic.sh
+++ src/bin/ffmpeg.cam.mic.sh
@@ -1,5 +1,7 @@
-#!/usr/bin/sh
+ . ~/datetime
 
-datetime=$( date +%Y%m%d%H%M%S )
-
-ffmpeg -f v4l2 -i /dev/video0 -f alsa -ac 2 -i hw:0 -q:v 1 -f flv "cam+mic.${datetime}.sh"
+ffmpeg -y \
+ -f v4l2 -i /dev/video0 \
+ -f alsa -ac 2 -i hw:0 \
+ -q:v 1 \
+ -f flv "cam.mic.${datetime}.sh"
