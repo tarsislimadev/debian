e177c4d91bd85222aa01c39a247d9c08d2d79a6b
Tarsis Lima <69215425+tarsislimadev@users.noreply.github.com>
2024-12-10 16:14:25 -0300

    Updating app files

--- src/install/postman.sh
+++ src/install/postman.sh
@@ -6,14 +6,10 @@ sudo echo OK
 echo
 echo Installing Postman
 echo
-curl -sL 'https://dl.pstmn.io/download/latest/linux64' -H 'Connection: keep-alive' -H 'Upgrade-Insecure-Requests: 1' -H 'User-Agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.61 Safari/537.36' -H 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9' -H 'Sec-GPC: 1' -H 'Sec-Fetch-Site: none' -H 'Sec-Fetch-Mode: navigate' -H 'Sec-Fetch-User: ?1' -H 'Sec-Fetch-Dest: document' -H 'Accept-Language: en-US,en;q=0.9,pt-BR;q=0.8,pt;q=0.7' --compressed --output ./postman.tar.gz
-tar -xf postman.tar.gz 
-mv Postman/ $HOME/.postman
-declare -x PATH=$HOME/.postman:$PATH
-rm -rf ./postman.tar.gz
+FIlENAME="postman.tar.gz"
+curl -sL 'https://dl.pstmn.io/download/latest/linux64' -H 'Connection: keep-alive' -H 'Upgrade-Insecure-Requests: 1' -H 'User-Agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.61 Safari/537.36' -H 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9' -H 'Sec-GPC: 1' -H 'Sec-Fetch-Site: none' -H 'Sec-Fetch-Mode: navigate' -H 'Sec-Fetch-User: ?1' -H 'Sec-Fetch-Dest: document' -H 'Accept-Language: en-US,en;q=0.9,pt-BR;q=0.8,pt;q=0.7' --compressed --output "${FIlENAME}"
+tar -xf "${FIlENAME}"
+mv Postman/ ${HOME}/.postman
+rm -rf "${FIlENAME}"
 
-echo
-echo Postman version
-echo
-
-echo [ ????? ]
+echo "Add ${HOME}/.postman to PATH"
