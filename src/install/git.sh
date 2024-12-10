721eae62264b102ea1e828ff4af930c2dfdf9761
Tarsis Lima <69215425+tarsislimadev@users.noreply.github.com>
2024-12-10 16:02:36 -0300

    Updating app files

--- src/install/git.sh
+++ src/install/git.sh
@@ -7,21 +7,13 @@ echo
 echo
 echo Installing Git
 echo
-sudo apt -y update
-sudo apt -y install git make
 
-echo
-echo Configuration of Git
-echo
-echo "name: "
-read name
-git config --global user.name "${name}"
-echo "email: "
-read email
-git config --global user.email "${email}"
-git config --global init.defaultBranch main
+sudo apt -y update
+
+sudo apt -y install git make
 
 echo
 echo Git version
 echo
+
 git --version
