1121fe3bbbfa5875fbb24c91890dd89a5ecbee7f
Tarsis Lima <69215425+tarsislimadev@users.noreply.github.com>
2024-12-10 16:00:11 -0300

    Updating app files

--- src/bin/ytdl.sh
+++ src/bin/ytdl.sh
@@ -1,3 +1,1 @@
-#!/usr/bin/sh
-
 docker run --rm -it --network host -v $( pwd ):/app -w /app tmvdl/yt-dl:dlp-alpine yt-dlp -o '%(uploader)s/%(playlist)s/%(playlist_index)s - %(title)s.%(ext)s' "${1}" 
