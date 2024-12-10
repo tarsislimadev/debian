814f1cb8a6d405ba1b47075b561c56c5fc94104c
Tarsis Lima <69215425+tarsislimadev@users.noreply.github.com>
2024-12-10 15:59:31 -0300

    Updating app files

--- src/bin/gemini.sh
+++ src/bin/gemini.sh
@@ -1,13 +1,11 @@
 #!/usr/bin/sh
 
-. ~/.env
+. ~/.generativelanguageapikey
 
 text="${1}"
 
 url="https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=${generativelanguageapikey}"
 
-curl -sSL \
- -X POST \
- -H "Content-Type: application/json" \
+curl -sSL -X POST -H "Content-Type: application/json" \
  -d "{'contents':[{'parts':[{'text':'${text}'}]}]}" \
  "${url}"
