237ddb9fd89c41cadc18525797fa432ec6dd07e9
Tarsis Lima <69215425+tarsislimadev@users.noreply.github.com>
2024-12-12 16:33:35 -0300

    Updating app files

--- src/bin/ffmpeg.cam.mic.sh
+++ src/bin/ffmpeg.cam.mic.sh
@@ -2,6 +2,6 @@
 
 ffmpeg -y \
  -f v4l2 -i /dev/video0 \
- -f alsa -ac 2 -i hw:0 \
- -q:v 1 \
- -f flv "cam.mic.${datetime}.sh"
+ -f alsa -ac 2 -i "" \
+ -filter:a "volume=30dB" -q:v 1 \
+ -f flv "cam+desktop+mic.${datetime}.flv"
