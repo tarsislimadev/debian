e339743dcfe6486c72719a36a9ca48dca5dfbb9c
Tarsis Lima <69215425+tarsislimadev@users.noreply.github.com>
2024-12-13 10:56:23 -0300

    Updating app files

--- src/install/flutter.sh
+++ src/install/flutter.sh
@@ -2,16 +2,22 @@
 
 echo Testing sudo
 sudo echo OK
-
-FILENAME="flutter_linux_3.27.0-stable.tar.xz"
-PATHNAME="~/.flutter"
+echo
 
 echo
 echo Installing Flutter
 echo
+FILENAME="flutter_linux_3.27.0-stable.tar.xz"
+PATHNAME="~/.flutter"
+
+mkdir -p "${PATHNAME}"
+
 sudo apt update -y
 sudo apt upgrade -y
 sudo apt install -y curl git unzip xz-utils zip libglu1-mesa clang cmake ninja-build pkg-config libgtk-3-dev liblzma-dev libstdc++-12-dev
+
 curl -O "https://storage.googleapis.com/flutter_infra_release/releases/stable/linux/${FILENAME}"
 tar -xf "${FILENAME}" -C "${PATHNAME}"
-echo "add ${PATHNAME} in PATH"
+rm -rf "${FILENAME}"
+
+echo "Add ${PATHNAME} in PATH"
