dfebfbd67169f5337111f7c0d1f33db5ce0b14d3
Tarsis Lima <69215425+tarsislimadev@users.noreply.github.com>
2024-12-14 11:15:34 -0300

    Updating app files

--- src/install/dart.sh
+++ src/install/dart.sh
@@ -4,12 +4,14 @@ echo Testing sudo
 sudo echo OK
 echo
 
+[[ -z "${DART_VERSION}" ]] && DART_VERSION="3.6.0"
+
 echo
 echo Installing Dart
 echo
 sudo apt -y update
-FILENAME="dart_3.6.0-1_amd64.deb"
-curl -sSLO "https://storage.googleapis.com/dart-archive/channels/stable/release/latest/linux_packages/${FILENAME}"
+FILENAME="dart_${DART_VERSION}-1_amd64.deb"
+curl -sSLO "https://storage.googleapis.com/dart-archive/channels/stable/release/${DART_VERSION}/linux_packages/${FILENAME}"
 sudo dpkg -i "${FILENAME}"
 rm -rf "${FILENAME}"
 dart --version
