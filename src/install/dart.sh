6e2ca43cb0ec2efca77b24cb0e7bfd8021fec9b9
Tarsis Lima <69215425+tarsislimadev@users.noreply.github.com>
2024-12-14 10:51:05 -0300

    Updating app files

--- src/install/dart.sh
+++ src/install/dart.sh
@@ -8,8 +8,8 @@ echo
 echo Installing Dart
 echo
 sudo apt -y update
-sudo apt -y install apt-transport-https
-wget -qO- https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo gpg  --dearmor -o /usr/share/keyrings/dart.gpg
-echo 'deb [signed-by=/usr/share/keyrings/dart.gpg arch=amd64] https://storage.googleapis.com/download.dartlang.org/linux/debian stable main' | sudo tee /etc/apt/sources.list.d/dart_stable.list
-sudo apt -y update
-sudo apt -y install dart
+FILENAME="dart_3.6.0-1_amd64.deb"
+curl -sSLO "https://storage.googleapis.com/dart-archive/channels/stable/release/latest/linux_packages/${FILENAME}"
+sudo dpkg -i "${FILENAME}"
+rm -rf "${FILENAME}"
+dart --version
