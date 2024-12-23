bc56540af7d1c076f439899493182466861d5ed3
🇧🇷 Tarsis Lima <69215425+tarsislimadev@users.noreply.github.com>
2024-12-22 21:12:19 -0300

    Updating app files

--- src/install/aws.sh
+++ src/install/aws.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 
+echo
 echo Testing sudo
 sudo echo OK
+echo
 
 echo
 echo Installing AWS CLI
 echo
-
+sudo apt -y update
 sudo apt -y install awscli ec2-instance-connect
 
 echo
 echo Version of AWS CLI
 echo
-
 aws --version
 
 echo
 echo Setting up
 echo
-
 echo 'export AWS_ACCESS_KEY_ID=' >> ~/.bashrc
 echo 'export AWS_SECRET_ACCESS_KEY=' >> ~/.bashrc
