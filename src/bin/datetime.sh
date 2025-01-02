512742d2a5e0bf89bc3c164841ec35e07c627553
🇧🇷 Tarsis Lima <69215425+tarsislimadev@users.noreply.github.com>
2025-01-02 15:29:05 -0300

    Updating app files

--- src/bin/datetime.sh
+++ src/bin/datetime.sh
@@ -1,5 +1,25 @@
-echo
-echo
-datetime=$( date +%Y%m%d%H%M%S )
-echo
-echo
+#!/usr/bin/sh
+
+# yyyy-MM-dd HH:mm:ss
+
+year=$( date +%Y )
+
+month=$( date +%m )
+
+day=$( date +%d )
+
+date="${year}-${month}-${day}"
+
+hour=$( date +%H )
+
+minute=$( date +%M )
+
+second=$( date +%S )
+
+time="${hour}:${minute}:${second}"
+
+timestamp="${date} ${time}"
+
+datetime="${year}${month}${day}${hour}${minute}${second}"
+
+[[ -n "${1}" ]] && echo "datetime: ${datetime}"
