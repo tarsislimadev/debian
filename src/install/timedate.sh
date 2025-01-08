d66472f2f331d6fcfca4d87178ab3247d5f4309a
Tarsis Lima <69215425+tarsislimadev@users.noreply.github.com>
2025-01-08 09:10:09 -0300

    Updating app files

--- src/install/timedate.sh
+++ src/install/timedate.sh
@@ -1,4 +1,4 @@
-D=$( curl -sSL "http://worldtimeapi.org/api/timezone/America/Sao_Paulo" | jq '.datetime' | sed -e 's/T/ /' | sed -e 's/\.............//ig' | sed -e 's/\"//ig' )
+D=$( curl -sSL "http://worldtimeapi.org/api/timezone/America/Sao_Paulo" | jq -r '.datetime' | sed -e 's/T/ /' | sed -e 's/\.............//ig' | sed -e 's/\"//ig' )
 
 echo sudo timedatectl set-time "${D}"
 
