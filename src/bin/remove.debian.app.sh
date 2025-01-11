9a27b520e30833fd646f8c0d4f1d23e2baf4be9a
Tarsis Lima <69215425+tarsislimadev@users.noreply.github.com>
2025-01-11 15:37:15 -0300

    Updating app files

--- src/bin/remove.debian.app.sh
+++ src/bin/remove.debian.app.sh
@@ -1,11 +1,9 @@
-sudo apt install -y net-tools
-sudo netstat --tcp --udp --listening --all --numeric --program --verbose
-
 sudo systemctl stop kdeconnect
 sudo systemctl disable kdeconnect
 
 sudo killall exim4 kdeconnectd
 
 sudo apt -y remove --purge cups* kdeconnect* exim4* bluetooth* xwayland* kwin-wayland* plasma-workspace-wayland* xwayland* dragonplayer* okular* libreoffice* gimp* imagemagick* gwenview*
+sudo apt remove --purge -y firefox* konqueror* libreoffice*
 
 sudo apt -y autoremove --purge
