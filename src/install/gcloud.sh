883abb87a99884a0d0c0169dc1e097cba491f8a5
Tarsis Lima <br.tmvdl@gmail.com>
2025-01-21 15:22:05 -0300

    Updating app files

--- src/install/gcloud.sh
+++ src/install/gcloud.sh
@@ -6,8 +6,10 @@ sudo echo OK
 echo
 echo Install Google Cloud CLI
 echo
-sudo apt -y install apt-transport-https ca-certificates gnupg
-echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://packages.cloud.google.com/apt -y cloud-sdk main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
-curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key --keyring /usr/share/keyrings/cloud.google.gpg add -
-sudo apt -y update
-sudo apt -y install google-cloud-cli
+FILENAME="google-cloud-cli-linux-x86_64.tar.gz"
+curl -O "https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/${FILENAME}"
+tar -xf "${FILENAME}"
+rm -rf  "${FILENAME}"
+SDKDIR="google-cloud-sdk/"
+mv "${SDKDIR}" "~/.${SDKDIR}"
+echo "export PATH=\"~/.${SDKDIR}bin/:${PATH}\" " >> .bashrc
