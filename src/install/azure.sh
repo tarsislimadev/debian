6b579932b3ce13479674983e6581812ebcc41f9f
Tarsis Lima <69215425+tarsislimadev@users.noreply.github.com>
2025-08-01 13:49:41 -0300

    Update azure.sh

--- src/install/azure.sh
+++ src/install/azure.sh
@@ -3,14 +3,12 @@
 echo
 echo Testing sudo
 sudo echo OK
-echo
 
 echo
 echo Installing AWS CLI
 echo
 sudo apt -y update
 sudo apt -y upgrade
-sudo apt -y install awscli
 sudo apt -y install ca-certificates curl apt-transport-https lsb-release gnupg
 curl -sL https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/microsoft.gpg > /dev/null
 echo "deb [arch=amd64] https://packages.microsoft.com/repos/azure-cli/ $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/azure-cli.list
