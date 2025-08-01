11a953fd544449bc6ddb68841e88ff1d759608dd
Tarsis Lima <69215425+tarsislimadev@users.noreply.github.com>
2025-08-01 13:43:38 -0300

    Create azure.sh

---
+++ src/install/azure.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+echo
+echo Testing sudo
+sudo echo OK
+echo
+
+echo
+echo Installing AWS CLI
+echo
+sudo apt -y update
+sudo apt -y upgrade
+sudo apt install -y awscli
+sudo apt install ca-certificates curl apt-transport-https lsb-release gnupg
+curl -sL https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/microsoft.gpg > /dev/null
+echo "deb [arch=amd64] https://packages.microsoft.com/repos/azure-cli/ $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/azure-cli.list
+sudo apt update
+sudo apt install azure-cli
+
+echo
+echo Version of AWS CLI
+echo
+az --version
