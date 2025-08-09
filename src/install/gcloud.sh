3f5858f2bd0ebda0daeda57b0fb785ed31334bec
Tarsis Lima <69215425+tarsislimadev@users.noreply.github.com>
2025-08-09 05:04:11 -0300

    Update gcloud.sh

--- src/install/gcloud.sh
+++ src/install/gcloud.sh
@@ -7,7 +7,7 @@ echo
 echo Install Google Cloud CLI
 echo
 FILENAME="google-cloud-cli-linux-x86_64.tar.gz"
-curl -O "https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/${FILENAME}"
+curl -s -O "https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/${FILENAME}"
 tar -xf "${FILENAME}"
 rm -rf  "${FILENAME}"
 SDKDIR="google-cloud-sdk/"
