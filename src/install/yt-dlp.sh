f92f74c82730fefbbb6cefe20e00932ee5921db0
Tarsis Lima <69215425+tarsislimadev@users.noreply.github.com>
2025-11-21 09:27:02 -0300

    Update yt-dlp.sh

--- src/install/yt-dlp.sh
+++ src/install/yt-dlp.sh
@@ -8,4 +8,4 @@ echo Installing Youtube Download
 echo
 sudo apt -y update
 sudo apt -y install python3-pip ffmpeg
-sudo python3 -m pip install --force-reinstall 'https://github.com/yt-dlp/yt-dlp/archive/master.tar.gz'
+sudo python3 -m pip install --force-reinstall 'https://github.com/yt-dlp/yt-dlp/archive/master.tar.gz' --break-system-packages
