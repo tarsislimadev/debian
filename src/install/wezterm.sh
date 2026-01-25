b1c8b63dcabf6cd954ed08b410811a4101ed0d88
Tarsis Lima <br.tmvdl@gmail.com>
2026-01-25 07:56:00 -0300

    Add WezTerm installer

---
+++ src/install/wezterm.sh
@@ -0,0 +1,7 @@
+curl -fsSL https://apt.fury.io/wez/gpg.key | sudo gpg --yes --dearmor -o /usr/share/keyrings/wezterm-fury.gpg
+echo 'deb [signed-by=/usr/share/keyrings/wezterm-fury.gpg] https://apt.fury.io/wez/ * *' | sudo tee /etc/apt/sources.list.d/wezterm.list
+sudo chmod 644 /usr/share/keyrings/wezterm-fury.gpg
+
+sudo apt -y update
+
+sudo apt install -y wezterm
